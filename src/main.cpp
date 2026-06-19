53f5b5cd4e5711a30197aedc0ec99ae343f2ecce
Ali Rouhi <thealirouhi@gmail.com>
2026-06-19 19:34:59 +0330

    feat: implement main entry point with Application initialization and run

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
-using namespace std;
+/*
+ * ========== Naming Convention Guideline ==========
+ * Class names    : PascalCase
+ * Function names : camelCase
+ * Variable names : lower_snake_case
+ * Constant names : UPPER_SNAKE_CASE
+ * =================================================
+ */
+
+#include "Application.h"
 
 int main()
 {
-    cout << "hello world!";
+    Application app;
+
+    if (!app.init()) {
+        return 1;
+    }
+
+    app.run();
 
     return 0;
 }
