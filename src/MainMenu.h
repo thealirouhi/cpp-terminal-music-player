626b5c140424ca085ac74a471c1992e232fda229
Yas <yas@example.com>
2026-06-19 19:25:43 +0330

    feat: implement MainMenu screen with banner, last played, and menu options

---
+++ src/MainMenu.h
@@ -0,0 +1,37 @@
+/*
+ * ========== Naming Convention Guideline ==========
+ * Class names    : PascalCase
+ * Function names : camelCase
+ * Variable names : lower_snake_case
+ * Constant names : UPPER_SNAKE_CASE
+ * =================================================
+ */
+
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include "Screen.h"
+#include "UIRenderer.h"
+#include "InputHandler.h"
+#include <string>
+using namespace std;
+
+class MainMenu : public Screen
+{
+public:
+    MainMenu(UIRenderer& renderer, InputHandler& input,
+             const string& lastPlayed);
+
+    void render() override;
+    void handleInput() override;
+
+    int getSelectedOption() const;
+
+private:
+    UIRenderer& renderer_;
+    InputHandler& input_;
+    string lastPlayed_;
+    int selectedOption_;
+};
+
+#endif
